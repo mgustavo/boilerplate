e1b9e2022fb497d873a6140a22c7799690870a99
Rafael Cavalcante <eu@rafaelcavalcante.com>
2013-05-16 16:10:00 -0300

    open graph adicionado

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		
+		<title></title>
+		<meta name="description" content="" />
+		<meta name="author" content="" />		
+		<meta name="keywords" content="" />
+		
+		<meta property="og:image" content="{url_da_imagem_de_share}" />
+		<meta property="og:title" content="{titulo_da_pagina}" />
+		<meta property="og:url" content="{endereço_canônico}" />
+		<meta property="og:site_name" content="{nome_do_site}" />
+		<meta property="og:type" content="{tipo_da_pagina}" />
+		<meta property="og:see_also" content="{url_relacionado}" />
+		
+		<!--[if lt IE 9]>
+			<script src="plugins/html5shiv.js"></script>			
+		<![endif]-->
+		
+		<link href="lin/reset.css" rel="stylesheet" type="text/css" />	
+		<link href="css/main.css" rel="stylesheet" type="text/css" />
+		
+	</head>
+	<body class="<?php echo $body_class ?>">
