335e3311df95beb20542eb4a3c3265302ae6b66d
Rafael Cavalcante <eu@rafaelcavalcante.com>
2013-05-16 16:09:50 -0300

    placeholder atualizado

--- plugins/lemonplaceholder.full.js
+++ plugins/lemonplaceholder.full.js
@@ -15,33 +15,33 @@
 
 function lemonPlaceholder() {
 
-var arraySuporte = ["ie7", "ie8", "ie9", "lt-ie9", "lt-ie8"]
-var arrayVersaoBrowser = document.getElementsByTagName("html")[0].getAttribute("class").split(" ");	
-var suporte = false;
+	var arraySuporte = ["ie7", "ie8", "ie9", "lt-ie9", "lt-ie8"]
+	var arrayVersaoBrowser = document.getElementsByTagName("html")[0].getAttribute("class").split(" ");	
+	var suporte = false;
 
-// Checa se o navegador se encaixa no que o usuário definiu
-for (x in arraySuporte) {
-if( arrayVersaoBrowser.indexOf( arraySuporte[x] ) != -1 ) {
-suporte = true;
-}
-}
+	// Checa se o navegador se encaixa no que o usuário definiu
+	for (x in arraySuporte) {
+		if( arrayVersaoBrowser.indexOf( arraySuporte[x] ) != -1 ) {
+			suporte = true;
+		}
+	}
 
-// aplica os valores correspondentes
-if (suporte) {	
-var campos = document.getElementsByClassName("inp-txt");
-var area = document.getElementsByClassName("inp-area");
-if (campos.length>0) {
-for (var i = 0; i < (campos.length); i++) {
-var valorPlaceholder = campos[i].getAttribute("placeholder");
-campos[i].setAttribute("value",valorPlaceholder);
-}
-}	
-if (area.length>0) {
-for (var i = 0; i < (area.length); i++) {
-var valorPlaceholder = area[i].getAttribute("placeholder");
-area[i].innerHTML=valorPlaceholder;
-}
-}	
-}	
+	// aplica os valores correspondentes
+	if (suporte) {	
+		var campos = document.getElementsByClassName("inp-txt");
+		var area = document.getElementsByClassName("inp-area");
+		if (campos.length>0) {
+			for (var i = 0; i < (campos.length); i++) {
+				var valorPlaceholder = campos[i].getAttribute("placeholder");
+				campos[i].setAttribute("value",valorPlaceholder);
+			}
+		}	
+		if (area.length>0) {
+				for (var i = 0; i < (area.length); i++) {
+				var valorPlaceholder = area[i].getAttribute("placeholder");
+				area[i].innerHTML=valorPlaceholder;
+			}
+		}	
+	}	
 
 }
