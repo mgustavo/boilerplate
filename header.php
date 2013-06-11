adef2f78d113bdad9000a65d8fec7ede5d5c16d9
Rafael Cavalcante <eu@rafaelcavalcante.com>
2013-06-11 14:55:36 -0300

    link relacional p/ o humans.txt adicionado ao header

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<link rel="author" href="humans.txt" />
 		
 		<title></title>
 		<meta name="description" content="" />
