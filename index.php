48f980adbca2bfc4eae860bfb149bfe09edf151e
Rafael Cavalcante <eu@rafaelcavalcante.com>
2013-07-01 11:25:26 -0300

    falso placeholder adicionado

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
-<?php $body_class = ""; include "header.php";
+<?php 
+	$body_class = "";
+	include "header.php";
 ?>	
 
-<!-- conteúdo aqui -->
+<input type="text" id="exemplo_de_input" placeholder="HEAUHEUEA"  style="border:1px solid #ccc;" />
+<label for="exemplo_de_input">LOLGO</label>
 
 <?php include "footer.php" ?>
--- js/main.js
+++ js/main.js
@@ -1,28 +1,48 @@
-$(document).ready(function(){
+function linksAtivos() {	
+	/* Insere a classe "ativo" nos links em estado ":active" */	
+	var baseURL = document.URL.split("/");
+	var posicaoNoArray = (baseURL.length)-1;
+	var urlDaPagina = baseURL[posicaoNoArray];
+	$("a[href='"+urlDaPagina+"']").addClass("ativo");	
+}
+
+function pseudoPlaceholder() {
+	$('input[type="text"], textarea').on('focus',function(){	
+		$('label[for="'+$(this).attr('id')+'"]').hide();		
+	});	
+	$('input[type="text"], textarea').on('blur',function(){	
+		if ( $(this).val() == '' ) {
+			$('label[for="'+$(this).attr('id')+'"]').show();		
+		}		
+	});	
+}
+
+$(document).on('ready',function(){
 
 	// Scroll para o topo / Voltar página anterior
-	$("#irTopo").click(function(){
+	$("#irTopo").on('click',function(){
 		$("html, body").animate({scrollTop:0}, 100);
 		return false;
 	});
-	$("#voltarPagina").click(function(){
-		history.back()
+	$("#voltarPagina").on('click',function(){
+		history.back();
 		return false;
 	});	
 	
 	if ( ($("a[rel=external]").length>0) ) { 
 		$('a[rel=external]').attr('target', '_blank');
-	}
+	}	
+	
+	linksAtivos();
+	
+	pseudoPlaceholder()
 	
 });
 
-$(window).load(function(){
-
-	/* Insere a classe "ativo" nos	links em estado ":active" */	
-	var baseURL = document.URL.split("/");
-	var posicaoNoArray = (baseURL.length)-1;
-	var urlDaPagina = baseURL[posicaoNoArray];
-	$("a[href='"+urlDaPagina+"']").addClass("ativo");
+$(window).on('load',function(){
+	
+	
 	
 });
+
 
