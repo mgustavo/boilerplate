aeb84be2a6d493ca7e7bc16bdb6b38ef8271a4a8
Rafael Cavalcante <eu@rafaelcavalcante.com>
2013-07-15 15:04:44 -0300

    error

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 	include "header.php";
 ?>	
 
-<input type="text" id="exemplo_de_input" placeholder="HEAUHEUEA"  style="border:1px solid #ccc;" />
-<label for="exemplo_de_input">LOLGO</label>
+<!-- conteúdo -->
 
 <?php include "footer.php" ?>
